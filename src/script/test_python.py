c786a765192f171e6ffa9d11a2495032efbe265c
BeneSell <bene6@hotmail.de>
2023-06-19 01:19:07 +0200

    adding and testing richt python output libaray

--- src/script/test_python.py
+++ src/script/test_python.py
@@ -5,21 +5,15 @@ import nltk
 nltk.download('punkt')
 # import sentence tokenizer from NLTK
 from nltk.tokenize import sent_tokenize
+import textwrap
 
+from rich.prompt import Prompt
+from rich.text import Text
+from rich.panel import Panel
+from rich import print
 
 
 hopefull_dicts = []
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 # give the one <i> tag which is the nearest to the % sign
@@ -33,7 +27,7 @@ Entrez.email = "bene6@hotmail.de"  # Always tell NCBI who you are
 abstracts_with_one_perc = []
 
 # Search PubMed for articles with "GC-content" in the title
-for i in range(0, 100, 20):
+for i in range(0, 60, 20):
     handle = Entrez.esearch(db="pubmed", term='("GC content of" OR "G + C content of") AND bacterial[Title/Abstract]', retstart=i, retmax=20)
     record = Entrez.read(handle)
     ids = record["IdList"]
@@ -87,21 +81,38 @@ for pre_x in abstracts_with_one_perc:
         break
 
     is_valuable_data = ""
-    print(bcolors.WARNING + "Abstract:" + bcolors.ENDC)
+    print("[bold yellow]" + "Abstract:" +"[/]")
     # highlight the regex matches
-    string_to_show = x.replace(hopefully_bacterial, bcolors.OKGREEN + hopefully_bacterial + bcolors.ENDC).replace(hopefully_percentage_for_bacterial, bcolors.OKGREEN + hopefully_percentage_for_bacterial + bcolors.ENDC)
-    print(string_to_show)
 
-    print(bcolors.WARNING + "Gathered Info from Abstract" + bcolors.ENDC)
+    
+    styled_text = Text(x, justify="Center")
+
+    # Mark the occurrences of hopefully_bacterial with the desired style
+    styled_text.highlight_regex(hopefully_bacterial, "bold green")
+
+    # Mark the occurrences of hopefully_percentage_for_bacterial with the desired style
+    styled_text.highlight_regex(hopefully_percentage_for_bacterial, "bold green")
+
+    # Print the styled text
+    print(Panel(styled_text, title="Abstract"))
+
+
+    print("[bold yellow]" + "Gathered Info from Abstract" +"[/]")
     
     while(is_valuable_data != "y" and is_valuable_data != "n" and is_valuable_data != "m"):
-        is_valuable_data = input("Is the data for "+ bcolors.OKGREEN + str(hopefully_bacterial) + bcolors.ENDC + " with "+ bcolors.OKGREEN + str(hopefully_percentage_for_bacterial) + bcolors.ENDC + " correct? (y/n/m)")
+        is_valuable_data = Prompt.ask("Is the data for "+ "[bold green]" + str(hopefully_bacterial) + "[/]" + " with "+ "[bold green]" + str(hopefully_percentage_for_bacterial) + "[/]" + " correct? (y/n/m)")
 
-    hopefull_dict = {hopefully_bacterial: hopefully_percentage_for_bacterial, "is_valuable_data": is_valuable_data}
-
+    hopefull_dict = {"hopeful_bacteria":hopefully_bacterial,
+                     "hopefull_perecentage": hopefully_percentage_for_bacterial, 
+                     "paper:doi": pre_x["ELocationID"],
+                     "paper:title": pre_x["ArticleTitle"],
+                     "is_valuable_data": is_valuable_data}
+    print(hopefull_dict["paper:doi"], hopefull_dict["paper:title"])
     hopefull_dicts.append(hopefull_dict)
 
 # show only dicts with a value and a key
 # hopefull_dicts = [x for x in hopefull_dicts if list(x.keys())[0] is not None and list(x.values())[0] is not None]
 hopefull_dicts
 # how do we verify?
+
+# safe data in this format: "paper:title","Bacteria","GC-Content","paper:doi"
